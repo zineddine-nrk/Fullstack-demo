8e9689dcbefdf2789e6b9db9f6fb257f08cfa6eb
Zineddine <nirakzineddine@gmail.com>
2025-12-12 20:40:54 +0100

    l'ajout des dockerfiles et docker-compose

--- backend/app.py
+++ backend/app.py
@@ -2,9 +2,17 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return "Backend is running zineddine"
+
 @app.route("/api/message")
 def message():
     return jsonify({"message": "Hello DevOps - version 1.0"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
+
+
+
