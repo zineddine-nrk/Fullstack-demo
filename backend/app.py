82b72c5b9cef9cfd9bc1f313ee8493a37b9f0783
Zineddine <nirakzineddine@gmail.com>
2025-12-19 23:29:10 +0100

    Last version 2.0

--- backend/app.py
+++ backend/app.py
@@ -8,7 +8,7 @@ def home():
 
 @app.route("/api/message")
 def message():
-    return jsonify({"message": "Hello DevOps - Last version "})
+    return jsonify({"message": "Hello DevOps - Last version 2.0 "})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
