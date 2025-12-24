1cc129171cf215c3341b7a9e854fa8e7fecd1b56
Zineddine <nirakzineddine@gmail.com>
2025-12-24 21:52:45 +0100

    version 2.2

--- backend/app.py
+++ backend/app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Backend is running zineddine nirak "
+    return "Backend is running  "
 
 @app.route("/api/message")
 def message():
-    return jsonify({"message": "Hello DevOps - Last version 2.2 "})
+    return jsonify({"message": "Hello DevOps - Last version 2.2a "})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
